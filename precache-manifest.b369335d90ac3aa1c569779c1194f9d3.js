2c6f679594cd15962aed2dab9a838809e5ec8fd3
João Pereira <joao.olipereira@outlook.pt>
2025-05-25 00:23:40 +0100

    Updates

---
+++ precache-manifest.b369335d90ac3aa1c569779c1194f9d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a383f494f5caa6b68adb150b6a47c4f4",
+    "url": "/numerology/index.html"
+  },
+  {
+    "revision": "c9f76ec8c621dda300c8",
+    "url": "/numerology/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/numerology/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/numerology/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "c9f76ec8c621dda300c8",
+    "url": "/numerology/static/js/main.345cd754.chunk.js"
+  },
+  {
+    "revision": "e997151aa4fae3e7ab5d",
+    "url": "/numerology/static/js/runtime~main.89f28f4f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/numerology/static/media/logo.06e73328.svg"
+  }
+]);
