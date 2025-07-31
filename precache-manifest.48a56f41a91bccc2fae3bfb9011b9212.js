2077426d79573eca09906cffb0c893c0b357551b
João Pereira <joao.olipereira@outlook.pt>
2025-07-31 01:30:04 +0100

    Updates

---
+++ precache-manifest.48a56f41a91bccc2fae3bfb9011b9212.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0c6aa8ecf79b33f2f6b7375d883f75",
+    "url": "/numerology/index.html"
+  },
+  {
+    "revision": "09ceda1cd8dcb88c0c14",
+    "url": "/numerology/static/css/main.a0090267.chunk.css"
+  },
+  {
+    "revision": "c38e352757e83f73074f",
+    "url": "/numerology/static/js/2.f77c50b8.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/numerology/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "09ceda1cd8dcb88c0c14",
+    "url": "/numerology/static/js/main.e962f3c9.chunk.js"
+  },
+  {
+    "revision": "3f89378c4055fc369b8d",
+    "url": "/numerology/static/js/runtime~main.8f780c8f.js"
+  },
+  {
+    "revision": "37560245632793b688997d626531ad87",
+    "url": "/numerology/static/media/enflag.37560245.png"
+  },
+  {
+    "revision": "9012812265871de518d9c12e8de8b796",
+    "url": "/numerology/static/media/ptflag.90128122.png"
+  },
+  {
+    "revision": "b34f483f32d3a0011ea94e3a2d175608",
+    "url": "/numerology/static/media/stars.b34f483f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/numerology/precache-manifest.3fe92b91612c0a629d7ad6aeaa37a23b.js"
+  "/numerology/precache-manifest.48a56f41a91bccc2fae3bfb9011b9212.js"
 );
 
 self.addEventListener('message', (event) => {
